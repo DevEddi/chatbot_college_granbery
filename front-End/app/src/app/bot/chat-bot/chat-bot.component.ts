afa6ed95dcc9fb901d81224caee514156818cd87
Edmilson <scovinowork@gmail.com>
2021-10-10 17:25:01 -0300

    table

--- front-End/app/src/app/bot/chat-bot/chat-bot.component.ts
+++ front-End/app/src/app/bot/chat-bot/chat-bot.component.ts
@@ -11,5 +11,43 @@ export class ChatBotComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  /*
+  //Processar Formulario
 
+  $('#tabelaPerguntas').submit(function (event) {
+    event.preventDefault();
+
+
+    //Criar formData
+    var formData = {
+
+      'nome_receita': $('#input_nome_receita').val(),
+      'valor_receita': $('#input_valor_receita').val(),
+      'descricao_receita': $('#input_descricao_receita').val()
+    };
+
+    $.ajax({
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Beaver ' + $.cookie('jwt_token'),
+      },
+      type: 'POST',
+      url: 'http://localhost:8080/api/receita/incluir',
+      data: JSON.stringify(formData),
+      dataType: 'json',
+      encode: true,
+      success: function (data) {
+        location.href = 'receita.html';
+
+      },
+      error: function (data) {
+        $('#div-alert-message').prepend(data.responseText);
+        $('#div-alert-message').fadeIn();
+      }
+
+    });
+
+  });
+
+  */
 }
