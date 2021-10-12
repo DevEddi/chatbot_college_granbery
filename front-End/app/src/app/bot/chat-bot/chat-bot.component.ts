1afd64daf537f0e05c9b914088ae6a6337e59544
Edmilson <scovinowork@gmail.com>
2021-10-12 18:50:22 -0300

    filtro e marcador de pagina

--- front-End/app/src/app/bot/chat-bot/chat-bot.component.ts
+++ front-End/app/src/app/bot/chat-bot/chat-bot.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {MatTableDataSource} from "@angular/material/table";
 import {ChatBotService} from "./chat-bot.service";
+import {MatSort} from "@angular/material/sort";
+import {MatPaginator} from "@angular/material/paginator";
 
 @Component({
   selector: 'app-chat-bot',
@@ -11,6 +13,10 @@ export class ChatBotComponent implements OnInit {
 
   displayedColumns = ['id', 'name', 'email'];
   dataSource!: MatTableDataSource<any>;
+
+  @ViewChild('paginator') paginator! : MatPaginator;
+  @ViewChild(MatSort) matSort! : MatSort;
+
   constructor(private service: ChatBotService) {
 
   }
@@ -18,8 +24,13 @@ export class ChatBotComponent implements OnInit {
   ngOnInit(): void {
     this.service.getUserData().subscribe((response: any) => {
       this.dataSource = new MatTableDataSource(response)
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this. matSort;
       console.log('response is', response);
     })
+  }
+  filterData($event : any){
+    this.dataSource.filter = $event.target.value;
   }
 
 }
