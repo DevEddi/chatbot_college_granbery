6e42b1f412883c36cca2adf22e53c0d007bd332f
Edmilson <scovinowork@gmail.com>
2021-10-13 16:42:22 -0300

    up

--- front-End/app/src/app/bot/ask-the-bot/ask-the-bot.module.ts
+++ front-End/app/src/app/bot/ask-the-bot/ask-the-bot.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DialogBotClientComponent } from './dialog-bot-client/dialog-bot-client.component';
 
 
 
 @NgModule({
   declarations: [
 
+  
+    DialogBotClientComponent
   ],
   imports: [
     CommonModule,
